42eea493379856c8f2352a2e5e419d8e43e36d5b
GustavoManganelli <GMangan1@jbssa.com>
2026-07-22 13:47:48 -0600

    Addign API setup

---
+++ Backend/acessibility/src/main/java/com/example/acessibility/AcessibilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.acessibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessibilityApplication.class, args);
+	}
+
+}
